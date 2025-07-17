af87c709a7650e49a671fd8b77e5901bd31bd295
Alexey <pixell390@gmail.com>
2025-07-17 15:54:27 +0300

    default API_BASE_URL

--- frontend/src/config/consts.ts
+++ frontend/src/config/consts.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080"
 export const PAGE_SIZE = 10
 
 export const ROLES = {
