937688f4deef3f1a7a46da58ece5512eea943ac8
Alexey <pixell390@gmail.com>
2025-07-21 13:45:49 +0300

    finalUrl fix

--- frontend/src/components/SearchDialogModal.tsx
+++ frontend/src/components/SearchDialogModal.tsx
@@ -11,7 +11,7 @@ import { cn } from "@/lib/utils"
 import { useTranslation } from "react-i18next"
 import { memo, useEffect, useRef, useState } from "react"
 import type { TUserHistory } from "@/types"
-import { useLocation, useNavigate, useSearchParams } from "react-router"
+import { useNavigate, useSearchParams } from "react-router"
 import { Button } from "./ui/button"
 import { useDeleteSearchHistory } from "@/hooks/useDeleteSearhHistory"
 import ProductFilters from "./ProductFilters/ProductFilters"
@@ -29,7 +29,6 @@ const SearchDialogModal = ({
   userHistory,
 }: TSearhInputProps) => {
   const navigate = useNavigate()
-  const { pathname } = useLocation()
   const { t } = useTranslation()
   const [searchParams, setSearchParams] = useSearchParams()
   const [selectedBrands, setSelectedBrands] = useState<string[]>(
@@ -91,7 +90,7 @@ const SearchDialogModal = ({
 
     setSearchParams(params, { replace: false })
 
-    const finalUrl = `${pathname}?${params.toString()}`
+    const finalUrl = `/?${params.toString()}`
 
     navigate(finalUrl)
   }
